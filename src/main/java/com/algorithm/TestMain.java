6ec9e535f99410fe544e220d82dca70938229d8c
zongchao <zongchao@wejoydata.com>
2023-02-19 00:05:30 +0800

    https://leetcode.cn/problems/swap-nodes-in-pairs/description/

--- src/main/java/com/algorithm/TestMain.java
+++ src/main/java/com/algorithm/TestMain.java
@@ -2,20 +2,16 @@ package com.algorithm;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.util.HashSet;
+import java.util.Set;
 
 public class TestMain {
 
     public static void main(String[] args) {
-//        int[] nums = {-3, -2, 0, -1};
-//        System.out.println(maxSubArray(nums));
-//        String a = "aa";
-
-        try {
-            InetAddress address = InetAddress.getLocalHost();
-            System.out.println(address.getHostAddress());
-        } catch (UnknownHostException e) {
-            e.printStackTrace();
-        }
+        char a='0';
+        int c=a;
+        System.out.println(c);
+        System.out.println(numDifferentIntegers("xtimt5kqkz9osexe56ezwwninlyeeqsq5m99904os3ygs12t31n1et4uwzmt5kvv6teisobuxt10k33v1aaxysg4y8nsivxdp3fo9dr7x58m8uc4ofm41ai77u8cvzr5r3s97f5otns59ubqk57xwl00xsp9w2oodt6yxcbscloyr9c2su8gca1ly6rrjufm25luhxhesxwn7bk1as9na4cbabxk"));
     }
 
     public static int maxSubArray(int[] nums) {
@@ -32,5 +28,35 @@ public class TestMain {
             }
         }
         return maxSum;
+    }
+
+    public static int numDifferentIntegers(String word) {
+        Set<String> set=new HashSet();
+        char[] chars=word.toCharArray();
+        String strNum="";
+        boolean existNum=false;
+        int size=0;
+        for(int i=0;i<chars.length;i++){
+            int temp=chars[i];
+            if(temp<57){
+                if(temp==48&!existNum){
+                    existNum=false;
+                    continue;
+                }
+                existNum=true;
+                strNum+=chars[i];
+                if (i==chars.length-1){
+                    set.add(strNum);
+                    continue;
+                }
+            }else{
+                if(existNum){
+                    set.add(strNum);
+                    strNum="";
+                }
+                existNum=false;
+            }
+        }
+        return set.size();
     }
 }
